ea3812c5739caf94bef932b89832b868a9e7d725
Zulfeqar1 <113031063+Zulfeqar1@users.noreply.github.com>
2024-03-01 13:25:20 +0100

    laget til kommentarer til forandringene

--- obj/Debug/net8.0/Ignist.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ignist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ignist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62caf60ef5b34a8164aa1342b9ba55c9d29c142a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54a0922d1eb98da8f2b1629614f5a979c4dac766")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ignist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ignist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
